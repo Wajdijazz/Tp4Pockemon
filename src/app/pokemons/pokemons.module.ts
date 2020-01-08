7e36f3a0a26c8ceccd077ffab086472c067dbc3a
Wajdijazz <mohamedwajdijaziri@gmail.com>
2020-01-08 11:51:40 +0100

    tp3

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -10,16 +10,28 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatButtonModule} from '@angular/material/button';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 const routes: Routes = [
-  { path: '', component: PokemonListComponent ,        
+  { path: '', component: PokedexComponent  , children: [
+        
+
+  { path: 'pokemon-list', component: PokemonListComponent  },
+
+    { path: 'pokemon-detail/:pokemonId', component: PokemonDetailComponent ,        
+  },
+  ]     
 },
-{ path: 'pokemon-detail/:pokemonId', component: PokemonDetailComponent ,        
-},
+
+
 ]
 
 @NgModule({
-  declarations: [PokemonListComponent, PokemonDetailComponent],
+  declarations: [PokemonListComponent, PokemonDetailComponent, PokedexComponent],
   imports: [
     CommonModule,
     MatListModule,
@@ -28,6 +40,10 @@ const routes: Routes = [
     MatChipsModule,
     MatButtonModule,
     InfiniteScrollModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
     RouterModule.forRoot(routes)
    
   ],
